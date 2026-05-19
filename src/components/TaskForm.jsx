db9fc5211336a4b1fe767141e81877e454adc523
SagarPatilLTM <sagar2.patil@ltm.com>
2026-05-19 10:47:51 +0530

    handle form elements individually

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,12 +3,27 @@ import "./TaskForm.css";
 import { Tag } from "./Tag";
 
 export const TaskForm = () => {
-  const [taskData, setTaskData] = useState({});
+  const [task, setTask] = useState("");
+  const handleTaskChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const [status, setStatus] = useState("todo");
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
+  console.log(task, status);
 
   return (
     <header className="app_header">
       <form>
-        <input className="task_input" type="text" name="task" />
+        <input
+          className="task_input"
+          type="text"
+          name="task"
+          onChange={handleTaskChange}
+        />
         <div className="task_form_bottom">
           <div>
             <Tag tagName="HTML" />
@@ -17,7 +32,11 @@ export const TaskForm = () => {
             <Tag tagName="React" />
           </div>
           <div>
-            <select name="status" className="task_status">
+            <select
+              name="status"
+              className="task_status"
+              onChange={handleStatusChange}
+            >
               <option value="todo">To Do</option>
               <option value="inprogress">In Progress</option>
               <option value="done">Done</option>
